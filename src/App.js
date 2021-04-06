287e3512efc4d9c30fb39fd5ab87e465dcf9a4ad
aditya3232 <m.aditya3232@gmail.com>
2021-04-06 13:07:42 +0700

    Membuat komponen Navbar & Button
    - memasukkan gambar & video ke folder public
    - membuat react functional export component dengan snippet rfce
    - install react router
    - copy CDN font awesome di folder public, index.html
    - file CDN ada di open account navigation web font awesome
    - add komponen Navbar.js ke App.js
    - membuat menu Navbar
    - membuat komponen Button
    - membuat import React dengan snippet imr
    - membuat styling App, Navbar, Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+// file yang dijadikan Router adalah App.js. Karena komponen inilah yang dirender pertama oleh index.js
+// fungsi Router adalah ketika halaman" diklik maka Router akan mengarahkan ke <Route> yang sesuai
+// singkatnya Router digunakan untuk menavigasi halaman
+// <Route> pasti dibungkus oleh <Switch>
+// didalam <Route> hanya bisa merender 1 path
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
+
+//
+function Navbar() {
+  // useState merupakan fungsi untuk mendapatkan data
+  // const [clik, setClik], ini adalah state (tempat menyimpan data)
+  // data yg disimpan adalah icon "fas fa-times" (x) : "fas fa-bars (bar3 =)"
+  // state clik akan menyimpan data basic (sebelum ada perubahan ketika di klik)
+  // dan state setClik menyimpan data setelah diubah
+  // useState(false) akan menjadikan state yg disimpan adalah false,
+  // karena false maka, state clik menyimpan data "fas fa-bars" dulu baru "fas fa-times",
+  // dan setClik akan menyimpan data click yg true (!click)
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  // fungsi handleClick akan berjalan ketika onClik
+  // yang dilakukan adalah merubah icon ketika di clik
+  // setClik(!clik), artinya setClick jadi (kebalikan state clik dan dapat dilakukan trus setiap diklik)
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  // menghilangkan sign up yg tidak tersembunyi pada tampilan windows ketika direload
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          {/* ini adalah link logo: TRVL (LOGO) */}
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            TRVL <i className="fab fa-typo3" />
+          </Link>
+
+          {/* ini adalah icon bars3 dan silang */}
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+
+          {/* ini adalah list menu navbar */}
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/product" className="nav-links" onClick={closeMobileMenu}>
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {/* Button SIGN UP */}
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
